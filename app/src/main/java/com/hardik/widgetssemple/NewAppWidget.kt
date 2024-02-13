9284fcec1252b8d39aafd4dac0bb50cdeb3bb5a8
HardikVaghani <hardik.abril@gmail.com>
2024-02-13 14:50:17 +0530

    Simple App Widget

---
+++ app/src/main/java/com/hardik/widgetssemple/NewAppWidget.kt
@@ -0,0 +1,41 @@
+package com.hardik.widgetssemple
+
+import android.app.PendingIntent
+import android.appwidget.AppWidgetManager
+import android.appwidget.AppWidgetProvider
+import android.content.Context
+import android.content.Intent
+import android.widget.RemoteViews
+
+class NewAppWidget : AppWidgetProvider() {
+    override fun onUpdate(context: Context, appWidgetManager: AppWidgetManager, appWidgetIds: IntArray) {
+        // There may be multiple widgets active, so update all of them
+        for (appWidgetId in appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId)
+        }
+    }
+
+    override fun onEnabled(context: Context) {
+        // Enter relevant functionality for when the first widget is created
+    }
+
+    override fun onDisabled(context: Context) {
+        // Enter relevant functionality for when the last widget is disabled
+    }
+}
+
+internal fun updateAppWidget(context: Context, appWidgetManager: AppWidgetManager, appWidgetId: Int) {
+    val widgetText = context.getString(R.string.appwidget_text)
+
+    val intent = Intent(context,MainActivity::class.java)
+    val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_IMMUTABLE)
+
+
+    // Construct the RemoteViews object
+    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
+    views.setTextViewText(R.id.appwidget_text, widgetText)
+    views.setOnClickPendingIntent(R.id.appwidget_text, pendingIntent)
+
+    // Instruct the widget manager to update the widget
+    appWidgetManager.updateAppWidget(appWidgetId, views)
+}
